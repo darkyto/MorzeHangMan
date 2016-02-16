59cb82304acad38ce352389acd6c5b0195123928
darkyto <iliev.nick007@gmail.com>
2016-02-16 15:40:16 +0200

    flashlight.android.js plugin installed + some test code added for toggle flash on and off. MARK: Genymotion not emulating lights.. need an android device to test.

--- platforms/android/src/main/assets/app/main-page.js
+++ platforms/android/src/main/assets/app/main-page.js
@@ -1,17 +1,31 @@
 
 var frameModule = require("ui/frame");
+var observableModule = require("data/observable");
+
+// testing purposes
+var flashlight = require("nativescript-flashlight"); 
+var viewModel = new observableModule.Observable();
 
 var topmost;
 
-
 function pageLoaded(args) {
     var page = args.object;
-
+	page.bindingContext = viewModel;
     topmost = frameModule.topmost();
 }
 
 function navToMenu() {
 	topmost.navigate("./menu");
+}
+
+viewModel.set("flashlightState", "Turn on");
+viewModel.toggleFlashlight = function() {
+	if (flashlight.isAvailable()) {
+        flashlight.toggle();
+        viewModel.set("flashlightState", (flashlight.isOn() ? "Turn off" : "Turn on"));
+    } else {
+        alert("A flashlight is not available on your device.");
+    }
 }
 
 exports.pageLoaded = pageLoaded;
