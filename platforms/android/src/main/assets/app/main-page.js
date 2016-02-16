d93df0d8d464f1f7749e5680a734ca010b34ab7d
darkyto <iliev.nick007@gmail.com>
2016-02-16 16:32:25 +0200

    basic splash screen animation added

--- platforms/android/src/main/assets/app/main-page.js
+++ platforms/android/src/main/assets/app/main-page.js
@@ -1,6 +1,8 @@
 
 var frameModule = require("ui/frame");
 var observableModule = require("data/observable");
+var viewModule = require("ui/core/view");
+var colorModule = require("color");
 
 // testing purposes
 var flashlight = require("nativescript-flashlight"); 
@@ -12,7 +14,30 @@ function pageLoaded(args) {
     var page = args.object;
 	page.bindingContext = viewModel;
     topmost = frameModule.topmost();
+
+    // this will animate the logo on initial load
+    var img = viewModule.getViewById(page, "logoImage");
+    img.scaleX = 0.1;
+    img.scaleY = 0.1;
+    img.animate({
+    	scale: { x: 1, y: 1},
+    	rotate: 360,
+    	duration: 3000
+    });
+
+    var heading = viewModule.getViewById(page, "headingLabel");
+    heading.scaleX = 0.1;
+    heading.scaleY = 0.1;
+    heading.rotate = 180;
+    heading.animate({
+    	scale: {x: 0.6, y: 0.6},
+    	duration: 3000
+    }).then(function () { return heading.animate({
+    	scale: {x: 1, y: 1}, 
+    	rotate: 0, 
+    	duration:400 }); });
 }
+
 
 function navToMenu() {
 	topmost.navigate("./menu");
