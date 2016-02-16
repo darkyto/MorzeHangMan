8e5682c70504459f8e34de5fe82dca6ba9e4c0c1
darkyto <iliev.nick007@gmail.com>
2016-02-16 14:33:40 +0200

    segmentedBar made to change content with event handller in code behind

--- app/menu.js
+++ app/menu.js
@@ -1,18 +1,36 @@
 
-var segmentModule = require("ui/segmented-bar");
 var observableModule = require("data/observable");
 
+var segmentedBarModule = require("ui/segmented-bar");
+var viewModule = require("ui/core/view");
+
+
 var pageModules = (function() {
+
+	var segmentedBar;
+	var page;
+	var testLabel;
 
 	var pageModules = {
 
 		pageLoaded:function (args) {
-		    var page = args.object;
-		}		
+		    page = args.object;
+		    segmentedBar = viewModule.getViewById(page, "segmentedBar");
+		    testLabel = viewModule.getViewById(page, "testLabel");
+
+
+			segmentedBar.on(observableModule.Observable.propertyChangeEvent, function(propertyChangeData){
+			  	console.log(propertyChangeData.propertyName + " has been changed and the new value is: " + propertyChangeData.value);
+
+			  	testLabel.set("text", propertyChangeData.value);
+			})
+
+		    console.log(segmentedBar.selectedIndex);
+		}
 	}
-	
 
 	return pageModules;
 })();
 
 exports.pageLoaded = pageModules.pageLoaded;
+
