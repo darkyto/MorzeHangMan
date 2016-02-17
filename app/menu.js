e3b7361d75c60da6e1c7d4b4c9e14b5bfabc8dd2
Victor Velchev <victor.velchev@gmail.com>
2016-02-17 12:22:35 +0200

    ListView of alphabet adjusted. Must position correctly at a later point.

--- app/menu.js
+++ app/menu.js
@@ -60,10 +60,10 @@ function onLoad(args) {
     console.log("in menu.js @ onLoad");
 
     var _morseAlphabetLabel = view.getViewById(page, "morseAlphabetLabel");
-    var _morseAlphabetListPicker = view.getViewById(page, "morseAlphabetListPicker");
+    var _morseAlphabetListView = view.getViewById(page, "morseAlphabetListView");
 
     //_morseAlphabetLabel.text = "OK!";
-    _morseAlphabetListPicker.items = ["A	.-   ", "B	-... ", "C	-.-. ", "D	-..  ", "E	.    ", "F	..-. ", "G	--.  ", "H	.... ", "I	..   ", "J	.--- ", "K	-.-  ", "L	.-.. ", "M	--   ", "N	-.   ", "O	---  ", "P	.--. ", "Q	--.- ", "R	.-.  ", "S	...  ", "T	-    ", "U	..-  ", "V	...- ", "W	.--  ", "X	-..- ", "Y	-.-- ", "Z	--.. ", "0	-----", "1	.----", "2	..---", "3	...--", "4	....-", "5	.....", "6	-....", "7	--...", "8	---..", "9	----."];
+    _morseAlphabetListView.items = ["A	.-   ", "B	-... ", "C	-.-. ", "D	-..  ", "E	.    ", "F	..-. ", "G	--.  ", "H	.... ", "I	..   ", "J	.--- ", "K	-.-  ", "L	.-.. ", "M	--   ", "N	-.   ", "O	---  ", "P	.--. ", "Q	--.- ", "R	.-.  ", "S	...  ", "T	-    ", "U	..-  ", "V	...- ", "W	.--  ", "X	-..- ", "Y	-.-- ", "Z	--.. ", "0	-----", "1	.----", "2	..---", "3	...--", "4	....-", "5	.....", "6	-....", "7	--...", "8	---..", "9	----."];
 }
 
 exports.onLoad = onLoad;
