5c66a09e993d9e02f45a72c7467b8a1f4cbd5ed0
darkyto <iliev.nick007@gmail.com>
2016-02-18 15:54:10 +0200

    TEXT to MORSE added with two-way binding - now we need to make the opposite

--- app/menu.js
+++ app/menu.js
@@ -8,6 +8,7 @@ var vibrator = require("nativescript-vibrate");
 var _dot;
 var _slash;
 var _morseCodeArray;
+var _decodedMorseArray;
 var morseDelay = 600;
 var model;
 
@@ -18,101 +19,251 @@ function onLoad(args) {
 	_dot = sound.create("~/res/morse-dot.mp3");
 	_slash = sound.create("~/res/morse-slash.mp3");
 
-	model = new observableModel.Observable(
-	{
-	  "sliderDelayMinValue": morseDelay,
-	  "sliderDelayMaxValue": 3 * morseDelay,
-	  "sliderDelayCurrentValue": morseDelay,
-	  "sliderVolumeMinValue": 0,
-	  "sliderVolumeMaxValue": 100,
-	  "sliderVolumeCurrentValue": 70
-	}
-	);
+	model = new observableModel.Observable({
+		"sliderDelayMinValue": morseDelay,
+		"sliderDelayMaxValue": 3 * morseDelay,
+		"sliderDelayCurrentValue": morseDelay,
+		"sliderVolumeMinValue": 0,
+		"sliderVolumeMaxValue": 100,
+		"sliderVolumeCurrentValue": 70,
+		"textToMorse" : "SOS",
+		"morseToText" : "... --- ..."
+	});
 
 	var _morseAlphabetLabel = view.getViewById(page, "morseAlphabetLabel");
 	var _morseAlphabetListView = view.getViewById(page, "morseAlphabetListView");
 
-  	_morseCodeArray = new observableModule.ObservableArray();
+	generateMorseObservableArray();
 
-	_morseCodeArray.push({letter: "LETTER", code: 'CODE'});
-
-	_morseCodeArray.push({letter: "A", code: '.-'});
-	_morseCodeArray.push({letter: "B", code: '-...'});
-	_morseCodeArray.push({letter: "C", code: '-.-.'});
-	_morseCodeArray.push({letter: "D", code: '-..'});
-	_morseCodeArray.push({letter: "E", code: '.'});
-	_morseCodeArray.push({letter: "F", code: '..-.'});
-	_morseCodeArray.push({letter: "G", code: '--.'});
-	_morseCodeArray.push({letter: "H", code: '....'});
-	_morseCodeArray.push({letter: "I", code: '..'});
-	_morseCodeArray.push({letter: "J", code: '.---'});
-
-	_morseCodeArray.push({letter: "K", code: '-.-'});
-	_morseCodeArray.push({letter: "L", code: '.-..'});
-	_morseCodeArray.push({letter: "M", code: '--'});
-	_morseCodeArray.push({letter: "N", code: '-.'});
-	_morseCodeArray.push({letter: "O", code: '---'});
-	_morseCodeArray.push({letter: "P", code: '.--.'});
-	_morseCodeArray.push({letter: "Q", code: '--.-'});
-	_morseCodeArray.push({letter: "R", code: '.-.'});
-	_morseCodeArray.push({letter: "S", code: '...'});
-	_morseCodeArray.push({letter: "T", code: '-'});
-
-	_morseCodeArray.push({letter: "U", code: '..-'});
-	_morseCodeArray.push({letter: "V", code: '...-'});
-	_morseCodeArray.push({letter: "W", code: '.--'});
-	_morseCodeArray.push({letter: "X", code: '-..-'});
-	_morseCodeArray.push({letter: "Y", code: '-.--'});
-	_morseCodeArray.push({letter: "Z", code: '--..'});
-
-	_morseCodeArray.push({letter: "0", code: '-----'});
-	_morseCodeArray.push({letter: "1", code: '.----'});
-	_morseCodeArray.push({letter: "2", code: '..---'});
-	_morseCodeArray.push({letter: "3", code: '...--'});
-	_morseCodeArray.push({letter: "4", code: '....-'});
-	_morseCodeArray.push({letter: "5", code: '.....'});
-	_morseCodeArray.push({letter: "6", code: '-....'});
-	_morseCodeArray.push({letter: "7", code: '--...'});
-	_morseCodeArray.push({letter: "8", code: '---..'});
-	_morseCodeArray.push({letter: "9", code: '----.'});
 
 	model.set("myMorseItems", _morseCodeArray);
 	page.bindingContext = model;
+	
 }
 
+function onEncodeButtonTap(args) {
+	var page = args.object.page;
+	var textInput = model.get("textToMorse");
+	var resultOutput = encodeTextToMorse(textInput);
+	console.log(resultOutput);
+
+	model.set("morseToText", resultOutput);
+}
+
+
 function onListViewTap(args) {
-  var itemIndex = args.index;
+	var itemIndex = args.index;
 
 	var currentItem = _morseCodeArray.getItem(itemIndex);
 	var codeString = currentItem['code'];
 	console.log(codeString);
 
 	for (var i = 0, len = codeString.length; i < len; i++) {
-  	if (codeString[i] === "-") {
-      timer.setTimeout(
-        function playMorseCode(){
-          _slash.play();
-          //TODO: Fix permissions prior to use!
-          console.log("Trying to vibrate for 500ms");
-          vibrator.vibration(500);
-          console.log(model.sliderCurrentValue);
+		if (codeString[i] === "-") {
+			timer.setTimeout(
+				function playMorseCode() {
+					_slash.play();
+					//TODO: Fix permissions prior to use!
+					console.log("Trying to vibrate for 500ms");
+					vibrator.vibration(500);
+					console.log(model.sliderCurrentValue);
 
-        }
-        , model.sliderDelayCurrentValue * (i + 1));
-  	} else if (codeString[i] === ".") {
-      timer.setTimeout(
-        function playMorseCode(){
-          _dot.play();
-          //TODO: Fix permissions prior to use!
-          console.log("Trying to vibrate for 250ms");
+				}, model.sliderDelayCurrentValue * (i + 1));
+		} else if (codeString[i] === ".") {
+			timer.setTimeout(
+				function playMorseCode() {
+					_dot.play();
+					//TODO: Fix permissions prior to use!
+					console.log("Trying to vibrate for 250ms");
 
-          vibrator.vibration(250);
-          console.log(model.sliderCurrentValue);
-        }
-        , model.sliderDelayCurrentValue * (i + 1));
-  	}
+					vibrator.vibration(250);
+					console.log(model.sliderCurrentValue);
+				}, model.sliderDelayCurrentValue * (i + 1));
+		}
 	}
+}
+
+function generateMorseObservableArray() {
+
+	_morseCodeArray = new observableModule.ObservableArray();
+
+	_morseCodeArray.push({
+		letter: "LETTER",
+		code: 'CODE'
+	});
+
+	_morseCodeArray.push({
+		letter: "A",
+		code: '.-'
+	});
+	_morseCodeArray.push({
+		letter: "B",
+		code: '-...'
+	});
+	_morseCodeArray.push({
+		letter: "C",
+		code: '-.-.'
+	});
+	_morseCodeArray.push({
+		letter: "D",
+		code: '-..'
+	});
+	_morseCodeArray.push({
+		letter: "E",
+		code: '.'
+	});
+	_morseCodeArray.push({
+		letter: "F",
+		code: '..-.'
+	});
+	_morseCodeArray.push({
+		letter: "G",
+		code: '--.'
+	});
+	_morseCodeArray.push({
+		letter: "H",
+		code: '....'
+	});
+	_morseCodeArray.push({
+		letter: "I",
+		code: '..'
+	});
+	_morseCodeArray.push({
+		letter: "J",
+		code: '.---'
+	});
+
+	_morseCodeArray.push({
+		letter: "K",
+		code: '-.-'
+	});
+	_morseCodeArray.push({
+		letter: "L",
+		code: '.-..'
+	});
+	_morseCodeArray.push({
+		letter: "M",
+		code: '--'
+	});
+	_morseCodeArray.push({
+		letter: "N",
+		code: '-.'
+	});
+	_morseCodeArray.push({
+		letter: "O",
+		code: '---'
+	});
+	_morseCodeArray.push({
+		letter: "P",
+		code: '.--.'
+	});
+	_morseCodeArray.push({
+		letter: "Q",
+		code: '--.-'
+	});
+	_morseCodeArray.push({
+		letter: "R",
+		code: '.-.'
+	});
+	_morseCodeArray.push({
+		letter: "S",
+		code: '...'
+	});
+	_morseCodeArray.push({
+		letter: "T",
+		code: '-'
+	});
+
+	_morseCodeArray.push({
+		letter: "U",
+		code: '..-'
+	});
+	_morseCodeArray.push({
+		letter: "V",
+		code: '...-'
+	});
+	_morseCodeArray.push({
+		letter: "W",
+		code: '.--'
+	});
+	_morseCodeArray.push({
+		letter: "X",
+		code: '-..-'
+	});
+	_morseCodeArray.push({
+		letter: "Y",
+		code: '-.--'
+	});
+	_morseCodeArray.push({
+		letter: "Z",
+		code: '--..'
+	});
+
+	_morseCodeArray.push({
+		letter: "0",
+		code: '-----'
+	});
+	_morseCodeArray.push({
+		letter: "1",
+		code: '.----'
+	});
+	_morseCodeArray.push({
+		letter: "2",
+		code: '..---'
+	});
+	_morseCodeArray.push({
+		letter: "3",
+		code: '...--'
+	});
+	_morseCodeArray.push({
+		letter: "4",
+		code: '....-'
+	});
+	_morseCodeArray.push({
+		letter: "5",
+		code: '.....'
+	});
+	_morseCodeArray.push({
+		letter: "6",
+		code: '-....'
+	});
+	_morseCodeArray.push({
+		letter: "7",
+		code: '--...'
+	});
+	_morseCodeArray.push({
+		letter: "8",
+		code: '---..'
+	});
+	_morseCodeArray.push({
+		letter: "9",
+		code: '----.'
+	});
+
+}
+
+function encodeTextToMorse(text) {
+
+	_decodedMorseArray = new observableModule.ObservableArray();
+
+	text = text.toUpperCase();
+
+	for (var i = 0, len = text.length; i < len; i++) {
+		for (var y = 0; y < _morseCodeArray.length; y++) {
+			var currentItem = _morseCodeArray.getItem(y);
+			var currentLetter = currentItem['letter'];
+
+			if (text[i] === currentLetter) {
+				_decodedMorseArray.push(currentItem['code']);
+			} else if (text[i] === " ") {
+				_decodedMorseArray.push(" ");
+			}
+		}
+	}
+
+	return _decodedMorseArray.join(' ');
 }
 
 exports.onLoad = onLoad;
 exports.onListViewTap = onListViewTap;
+exports.onEncodeButtonTap = onEncodeButtonTap;
